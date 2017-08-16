330a8742e9f2d0caa86c1e617ffc9b3fadce520d
Jonas <jonas_biedermann@hotmail.com>
2017-08-16 17:19:00 +0200

    Testing Files Consistent

--- mn-archetype-gatewayservice/src/main/resources/archetype-resources/src/test/java/ServiceTest.java
+++ mn-archetype-gatewayservice/src/main/resources/archetype-resources/src/test/java/ServiceTest.java
@@ -1,7 +1,7 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package SomeGame.${artifactId};
+package ${package};
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -10,7 +10,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class ServiceTest 
     extends TestCase
 {
     /**
@@ -18,7 +18,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public ServiceTest( String testName )
     {
         super( testName );
     }
@@ -28,26 +28,14 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( ServiceTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testService()
     {
         assertTrue( true );
     }
-    
-	public void testLogin() {
-//		CredentialValues creds = new CredentialValues();
-//		creds.setUsername("");
-//		creds.setPassword("");
-//		
-//		Request r = new Request();
-//		r.getParameters().set(ParameterCode.USER_REQUEST, "mn://account/login");
-//		r.setData(Serialization.serialize(creds));
-//		
-//		clientRequest(context, "connID42x", r);
-	}
 }
